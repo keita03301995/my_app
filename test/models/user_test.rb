3848dd4afcd22447edac84106f870778d740ee28
keita03301995 <keita03301995@gmail.com>
2016-03-01 15:46:45 +0900

    メールアドレスのテストの追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -41,7 +41,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "不正なメールアドレスが弾かれている事の確認" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -54,8 +54,15 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
-  test "パスワードが空でない事の確認" do
-    @user.password = @user.password_confirmation = "" * 6
+  test "メールアドレスがデータベースに小文字で登録されている事の確認" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "パスワードが空白文字列でない事の確認" do
+    @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
 
